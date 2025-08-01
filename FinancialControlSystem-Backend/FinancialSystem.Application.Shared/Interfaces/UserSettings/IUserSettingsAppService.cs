e9de37c4c3197ddf47bc88df6b5172f69c79fdf4
Lucas Mendes Israel <lukasm.israel@gmail.com>
2025-08-01 20:30:33 -0300

    Feature_05 - lógica de login do usuário com jwt

--- FinancialControlSystem-Backend/FinancialSystem.Application.Shared/Interfaces/UserSettings/IUserSettingsAppService.cs
+++ FinancialControlSystem-Backend/FinancialSystem.Application.Shared/Interfaces/UserSettings/IUserSettingsAppService.cs
@@ -1,11 +1,10 @@
 ﻿using FinancialSystem.Application.Shared.Dtos.User;
-using FinancialSystem.Core.Entities;
 
 namespace FinancialSystem.Application.Shared.Interfaces.UserSettings
 {
     public interface IUserSettingsAppService
     {
-        Task RegisterAsync(UserDataDto input);
-        Task<User?> LoginAsync(string email, string password);
+        Task RegisterUser(UserDataDto input);
+        Task<string> UserLogin(UserDataDto input);
     }
 }
