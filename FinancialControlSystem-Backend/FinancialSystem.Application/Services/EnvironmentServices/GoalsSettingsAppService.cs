7387ef3248bcd87cf529322d20ac3b1f2d046270
Lucas Mendes Israel <lukasm.israel@gmail.com>
2025-09-14 18:43:59 -0300

    Feature_22 - Feito Tdd para lógicas de Metas

---
+++ FinancialControlSystem-Backend/FinancialSystem.Application/Services/EnvironmentServices/GoalsSettingsAppService.cs
@@ -0,0 +1,19 @@
+﻿using FinancialSystem.Application.Shared.Interfaces;
+using FinancialSystem.Application.Shared.Interfaces.EnvironmentServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialSystem.Application.Services.EnvironmentServices
+{
+    public class GoalsSettingsAppService : AppServiceBase, IGoalsSettingsAppService
+    {
+
+
+        public GoalsSettingsAppService(IAppSession appSession) : base(appSession)
+        {
+        }
+    }
+}
---
+++ FinancialControlSystem-Backend/FinancialSystem.Test/EnvironmentTests/GoalsServiceTests.cs
@@ -0,0 +1,47 @@
+﻿namespace FinancialSystem.Test.EnvironmentTests
+{
+    public class GoalsServiceTests
+    {
+        [Fact]
+        public async Task ShouldInsertGoalSuccessfully()
+        {
+
+        }
+
+        [Fact]
+        public async Task ShouldExceptionWhenGoalInsertFails()
+        {
+
+        }
+
+        [Fact]
+        public async Task ShouldUpdateGoalSuccessfully()
+        {
+
+        }
+
+        [Fact]
+        public async Task ShouldExceptionWhenGoalNotFoundOnUpdate()
+        {
+
+        }
+
+        [Fact]
+        public async Task ShouldDeleteGoalSuccessfully()
+        {
+
+        }
+
+        [Fact]
+        public async Task ShouldThrowExceptionWhenDeleteGoalNotFound()
+        {
+
+        }
+
+        [Fact]
+        public async Task ShouldReturnListOfGoals()
+        {
+
+        }
+    }
+}
