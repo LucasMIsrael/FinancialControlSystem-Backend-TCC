8fae97496c5ad2c75fc7d4dd94458f44d25060a0
Lucas Mendes Israel <lukasm.israel@gmail.com>
2025-10-04 19:20:49 -0300

    Feature_28/29 - feito Tdd e lógicas para dashboards

---
+++ FinancialControlSystem-Backend/FinancialSystem.Application.Shared/Interfaces/EnvironmentServices/IDashboardsAppService.cs
@@ -0,0 +1,15 @@
+﻿using FinancialSystem.Application.Shared.Dtos.Environment;
+
+namespace FinancialSystem.Application.Shared.Interfaces.EnvironmentServices
+{
+    public interface IDashboardsAppService
+    {
+        Task<FinancialSummaryDto> GetFinancialSummary();
+        Task<List<TopRecurringGoalsAchievedDto>> GetTheFourMostAchievedRecurringGoals();
+        Task<UnexpectedExpensesAnalysisDto> GetUnexpectedExpensesAnalysis();
+        Task<GoalsSummaryDto> GetGoalsSummary();
+        Task<List<BalanceOverTimeDto>> GetBalanceOverTime(FilterForBalanceOverTimeDto input);
+        Task<List<AchievementsDistributionDto>> GetAchievementDistributionByPeriod();
+        Task<List<ProjectedBalanceDto>> GetProjectedBalanceEvolution(FiltersForBalanceProjectionDto input);
+    }
+}
